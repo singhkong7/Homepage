41f4adc236b3e775db6267637159954a4f7db62e
singhkong7 <singhpriyansh2014@gmail.com>
2021-06-23 22:57:19 +0530

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        https://www.aesop.com/<br></br>
+        - You will need to make a webpage using Reactjs and css to make this. It should be pixel perfect<br></br> 
+        ( it should match the original page exactly)<br></br>
+        ( we just want 1 page ? the homepage to be implemented)<br></br>
+        You can inspect the original webpage and get all the assets you need.<br></br>
+      </p>
+      <p>
+        Remember: You can take 24 to 48 hours to do this task. Just do it completely. Try to make your own <br></br>
+        logical assumptions and finish the task as best as you can. I want to see how you make decisions and work towards<br></br>
+        a solution ON YOUR OWN.<br></br>
+      </p>
+      <p>
+        Once you have the results please show me by sending a link on a external drive.
+      </p>
     </div>
   );
 }
